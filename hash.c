4f1a18d71cb544bd36a905dc343a072f62025f22
Utilisateur <user@debian.arcahe.ovh>
2019-11-08 09:13:53 +0100

    ok

--- hash.c
+++ hash.c
@@ -5,20 +5,32 @@
 int hacher(char *s);
 int main(void)
 {   printf("%d\n", 'A' + 'A' );
+	printf("##########################\n");
     printf("AA : %2d\n", hacher("AA"));
     printf("AC : %2d\n", hacher("AC"));
     printf("AG : %2d\n", hacher("AG"));
     printf("AT : %2d\n", hacher("AT"));
     printf("A- : %2d\n", hacher("A-"));
+    printf("##########################\n");
+    printf("CA : %2d\n", hacher("CA"));
     printf("CC : %2d\n", hacher("CC"));
     printf("CG : %2d\n", hacher("CG"));
-    printf("TC : %2d\n", hacher("TC"));
+    printf("CT : %2d\n", hacher("CT"));
     printf("C- : %2d\n", hacher("C-"));
+    printf("##########################\n");
+    printf("GA : %2d\n", hacher("GA"));
+    printf("GC : %2d\n", hacher("GC"));
     printf("GG : %2d\n", hacher("GG"));
-    printf("TG : %2d\n", hacher("TG"));
+    printf("GT : %2d\n", hacher("GT"));
     printf("G- : %2d\n", hacher("G-"));
+    printf("##########################\n");
+    printf("TA : %2d\n", hacher("TA"));
+    printf("TC : %2d\n", hacher("TC"));
+    printf("TG : %2d\n", hacher("TG"));
     printf("TT : %2d\n", hacher("TT"));
     printf("T- : %2d\n", hacher("T-"));
+    printf("-- : %2d\n", hacher("--"));
+    printf("##########################\n");
     return EXIT_SUCCESS;
 }
 
@@ -28,8 +40,8 @@ int hacher(char *s) {
 	len = strlen(s);
 	accum = 0;
 	for (;len > 0; len--) {
-		accum >>= 1;
+		accum <<=1;
 		accum += (unsigned) (*s++ & 0xFF);        
 	}
-	return (accum - 77)%15;
+	return (accum)%7;
 }
